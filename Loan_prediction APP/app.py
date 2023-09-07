ef50b80e7b5cfc26a8362a4e394d6c4d547fecb5
elanssariyassine <yassineelanssari0@gmail.com>
2023-09-07 11:42:47 +0100

    done

--- Loan_prediction APP/app.py
+++ Loan_prediction APP/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model_vf.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -19,8 +19,11 @@ def predict():
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
+    # Convert the prediction to "NO" for 0 and "YES" for 1
+    output = "Can" if prediction[0] == 1 else "Can't"
 
-    return render_template('index.html', prediction_text='La réponse est  $ {}'.format(output))
+    return render_template('index.html', prediction_text=f'This person {output} obtain a loan from the bank')
+
 
 
 
